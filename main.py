4ab4d3119e43589ba99c3620361fb9c6da6a9bbe
Kamil Pawlicki <kamilpawlicki1998@gmail.com>
2021-05-21 12:42:16 +0200

    pełna funkcjonalność oparta o zmienną wewnątrz kodu | skończone testy dodane opisy | przeniesiono BaseModel

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from pydantic import BaseModel, constr, NonNegativeInt, PositiveInt
 from pytz import timezone
 import json
 import urllib.request
+from raport_basemodel import *  # modele porzyjmowanych danych
 
 
 app = FastAPI()
@@ -27,41 +28,6 @@ app.Date_Format = "%Y-%m-%dT%H:%M:%S%z"  # format do jakiego ma być przekształ
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     return PlainTextResponse(str(exc), status_code=400)
-
-
-class PayByLink(BaseModel):
-    customer_id: Optional[PositiveInt] = None
-    created_at: str
-    currency: str  # Literal['EUR', 'USD', 'GBP', 'PLN']
-    amount: NonNegativeInt
-    description: str
-    bank: str
-
-
-class Dp(BaseModel):
-    customer_id: Optional[PositiveInt] = None
-    created_at: str
-    currency: str  # Literal['EUR', 'USD', 'GBP', 'PLN']
-    amount: NonNegativeInt
-    description: str
-    iban: constr(max_length=22, min_length=22)
-
-
-class Card(BaseModel):
-    customer_id: Optional[PositiveInt] = None
-    created_at: str
-    currency: str  # Literal['EUR', 'USD', 'GBP', 'PLN']
-    amount: NonNegativeInt
-    description: str
-    cardholder_name: str
-    cardholder_surname: str
-    card_number: constr(max_length=16, min_length=16)
-
-
-class RequestReport(BaseModel):
-    pay_by_link: Optional[List[PayByLink]]
-    dp: Optional[List[Dp]]
-    card: Optional[List[Card]]
 
 
 # funkcja przekształcająca pay_by_link dla klijenta
